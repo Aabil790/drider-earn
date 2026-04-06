af9e02bdd4ab3e16ccc60641e96556a65b2ff900
Aabil790 <aabil790@gmail.com>
2026-04-06 16:57:23 +0530

    Refactor Firebase setup for authentication
    Removed analytics import and initialization, added authentication import.

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,21 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCJSGFvPIgkdp_gbfOcMDs-dRxoDW7L56U",
   authDomain: "drider-earn.firebaseapp.com",
   projectId: "drider-earn",
   storageBucket: "drider-earn.firebasestorage.app",
   messagingSenderId: "119142900994",
-  appId: "1:119142900994:web:738e2e4266eed4e6f24e1b",
-  measurementId: "G-465BJQJFSV"
+  appId: "1:119142900994:web:738e2e4266eed4e6f24e1b"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);
